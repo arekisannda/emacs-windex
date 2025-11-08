60a7a94bf27a248e3c6aee77834f7137cfb2a465
Alex <alex@arekisannda.dev>
2025-11-07 21:33:25 -0800

    refator: remove `windex-state` feature

---
+++ test/test-windex.el
@@ -0,0 +1,14 @@
+;;; test-windex.el --- Tests for windex-state -*- lexical-binding: t -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'windex-utils)
+(require 'windex-layout)
+(require 'windex-purpose)
+(require 'windex-windmove)
+(require 'windex)
+(require 'ert)
+
+;;; test-windex.el ends here
